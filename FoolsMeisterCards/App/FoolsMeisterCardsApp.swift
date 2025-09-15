64831a3ed97f9605e0743085e13ea7aabcc36862
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-15 10:49:44 +0300

    save

---
+++ FoolsMeisterCards/App/FoolsMeisterCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoolsMeisterCardsApp.swift
+//  FoolsMeisterCards
+//
+//  Created by Serhii Babchuk on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoolsMeisterCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FCLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-FoolsMeisterCards/Pods-FoolsMeisterCards-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoolsMeisterCards : NSObject
+@end
+@implementation PodsDummy_Pods_FoolsMeisterCards
+@end
